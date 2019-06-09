95c65cd58793a62ede69ef11de99780aded6e381
Drew Stanger <drew.stanger@gmail.com>
2019-06-09 16:02:03 -0700

    finalized read me

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
-  $("form#vacationsurvey").submit(function(event){
-    event.preventDefault();
-    evalQuestion($("#weather").val());
-    evalQuestion($("#food").val());
-    evalQuestion($("#activity").val());
-    evalQuestion($("#sport").val());
-    evalQuestion($("#day").val());
-    $("#result").text(returnResult());
+  $('#vacationsurvey form').submit(function() {
+    evalQuestion($('#weather').val());
+    evalQuestion($('#food').val());
+    evalQuestion($('#activity').val());
+    evalQuestion($('#sport').val());
+    evalQuestion($('#day').val());
+    $('#result').text(returnResult());
   });
 });
 
@@ -15,23 +14,23 @@ var canadaCount=0;
 var seattleCount=0;
 
 var evalQuestion = function (res) {
-  if (result="mexico") mexicoCount++;
-  if (result="canada") canadaCount++;
-  if (result="seattle") seattleCount++;
+  if (result==='mexico') mexicoCount++;
+  if (result==='canada') canadaCount++;
+  if (result==='seattle') seattleCount++;
 }
 
 var returnResult = function () {
   if (mexicoCount>canadaCount && mexicoCount>seattleCount) {
-    $("#mexico").show();
-    $("#canada").hide();
-    $("#seattle").show();
+    $('#mexico').show();
+    $('#canada').hide();
+    $('#seattle').show();
   } else if (canadaCount>mexicoCount && canadaCount>seattleCount) {
-    $("#canada").show();
-    $("#mexico").hide();
-    $("#seattle").hide();
+    $('#canada').show();
+    $('#mexico').hide();
+    $('#seattle').hide();
   } else if (seattleCount>mexicoCount && seattleCount>canadaCount) {
-    $("#seattle").show();
-    $("#mexico").hide();
-    $("#canada") .hide();
+    $('#seattle').show();
+    $('#mexico').hide();
+    $('#canada') .hide();
   }
 }
